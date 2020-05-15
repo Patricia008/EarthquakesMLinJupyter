5e9916b3babb1b7e4a9d716941e98d92ebcf3f7f
Patricia008 <petrut.patricia@gmail.com>
2020-05-16 02:24:45 +0300

    Added regions and small adjustments to ml coefficients

--- src/config/parameterConfig.ts
+++ src/config/parameterConfig.ts
@@ -3,7 +3,7 @@ import { regions } from '../config/regionCoordinates'
 
 dotenv.config()
 
-const startDate = new Date(2020, 3, 1)
+const startDate = new Date(2018, 6, 23)
 const endDate = new Date(1900, 3, 1)
 
 export const algoEnum = {
@@ -17,11 +17,11 @@ const maxGraphSize = 2000
 
 export default {
 	USGS_API_URL: process.env.USGS_API_URL,
-	START_POINT: regions.CHILE,
+	START_POINT: regions.NEW_ZEALAND,
 	STARTTIME: startDate.toISOString(),
 	ENDTIME: endDate.toISOString(),
 	LIMIT: maxGraphSize < 2000 ? '' + maxGraphSize : '2000',
-	RADIUSES: ['50', '200', '500', '1000', '1500', '3000', '5000', '8000'],
+	RADIUSES: ['50', '200', '500', '1000', '1500', '3000', '5000'],
 	MAX_GRAPH_SIZE: maxGraphSize,
 	MAX_NR_OF_CHILDREN: maxNrOfChildren,
 	DAYS_BETWEEN_QUAKES: 10000,
--- src/config/regionCoordinates.ts
+++ src/config/regionCoordinates.ts
@@ -3,6 +3,11 @@ export enum regions {
 	ROMANIA = 'Romania',
 	CHILE = 'Chile',
 	MEDITERANEAN_SEA = 'MediteraneanSee',
+	NEW_GUINEA = 'NewGuinea',
+	S_ATLANTIC_OCEAN = 'SAtlanticOcean',
+	ALASKA = 'Alaska',
+	RUSSIA = 'Russia',
+	NEW_ZEALAND = 'NewZealand',
 }
 
 export const coordinates = {
@@ -21,5 +26,25 @@ export const coordinates = {
 	MediteraneanSee: {
 		lat: '34.394235',
 		long: '15.7975',
+	},
+	NewGuinea: {
+		lat: '-4.454000',
+		long: '154.713000',
+	},
+	SAtlanticOcean: {
+		lat: '-59.921000',
+		long: '-25.553000',
+	},
+	Alaska: {
+		lat: '70.024000',
+		long: '-142.724000',
+	},
+	Russia: {
+		lat: '54.976000',
+		long: '111.805000',
+	},
+	NewZealand: {
+		lat: '-38.739782',
+		long: '176.074958',
 	},
 }
